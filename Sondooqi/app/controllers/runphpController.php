26f6fbbb62c60ed12778c0f9b67fededd491263f
Sultan Abu Ghazal <sultanmobeen@hotmail.com>
2017-08-05 12:32:08 +0400

    computer lost its mind

--- Sondooqi/app/controllers/runphpController.php
+++ Sondooqi/app/controllers/runphpController.php
@@ -2,12 +2,19 @@
 
 class Runphp extends Controller {
 	public function defaultMethod(){
-        return;
-            $userModel = $this->model('UserModel');
-            $userModel->changeUserPassword($_SESSION['user_identification'], "12345", "1234");
-            print_r($userModel->getErrors());
-        //No default method, get home view
-        header("Location: ".$GLOBALS['webhost']['base_url']."/home");
-        exit;
+        require_once "..app\assets\lib";
+        $credentials = new Nexmo\Client\Credentials\Basic('ffcac206', 'ea4de7f43bca523f');
+        $client = new Nexmo\Client($credentials);
+        print_r($client);
+        
+        /*
+        $message = $client->message()->send([
+        'from' => 'Nexmo',
+        'to' => 'NUMBER_TO',
+        'text' => 'A text message sent using the Nexmo SMS API'
+        ]);
+        echo "Sent message to " . $message['to'] . ". Balance is now " . $message['remaining-balance'] . PHP_EOL;
+        */
+        echo "Hello World!";
     }
 }
